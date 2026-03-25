a731d4ec696d28bbe926f6ee38011c09c17103d1
GenJune <julia.nepochatova@gmail.com>
2026-03-25 15:02:12 +0300

    test for default constructed vector

--- pe-vector.h
+++ pe-vector.h
@@ -2,17 +2,29 @@
 #define PE_VECTOR_H
 #include <cstddef>
 
-namespace knk
-{
-  template <class T>
+namespace knk {
+  template < class T >
   class Vector {
   public:
     ~Vector();
     Vector();
 
+    bool isEmpty() const noexept;
+
   private:
     T* data_;
     size_t size_, capacity_;
   };
+}
+template < class T >
+knk::Vector < T >::Vector():
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
+{};
+
+template < class T >
+knk::Vector < T >::~Vector() {
+  delete [] data_;
 }
 #endif
