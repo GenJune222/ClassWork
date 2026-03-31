b278a9f0094c5477aa8ea9adadf4312ae9c2323e
GenJune <julia.nepochatova@gmail.com>
2026-03-31 22:16:09 +0300

    PopBack, pushBack and getCapacity methods implemented

--- pe-vector.h
+++ pe-vector.h
@@ -17,7 +17,7 @@ namespace knk {
     size_t getSize() const noexcept;
 
     void pushBack(const T&);
-    void popBack();
+    void popBack() noexcept;
     size_t getCapacity() const noexcept;
 
   private:
@@ -48,4 +48,36 @@ knk::Vector < T >::Vector(size_t size, const T& value):
    size_(0),
    capacity_(size)
 {};
+
+template < class T >
+size_t knk::Vector < T >::getCapacity() const noexcept {
+  return capacity_;
+}
+
+template < class T >
+void knk::Vector < T >::pushBack(const T& value) {
+  if (size_ >= capacity_) {
+    size_t newCapacity = capacity_ == 0 ? 1 : capacity_ * 2;
+    T* newData = new T[newCapacity];
+    try {
+      for (size_t i = 0; i < size_; ++i) {
+        newData[i] = data_[i];
+      }
+    } catch (...) {
+      delete [] newData;
+      throw;
+    }
+    delete [] data_;
+    data_ = newData;
+    capacity_ = newCapacity;
+  }
+  data_[size_++] = value;
+}
+
+template < class T >
+void knk::Vector < T >::popBack() noexcept {
+  if (size_ > 0) {
+    --size_;
+  }
+}
 #endif
